38cde3c4af16be03ae2617f9bc6025ea94c391aa
leticea <nascimento.leticeah@gmail.com>
2023-04-05 17:19:22 -0300

    feat(script.js): inserting IMask formatter

---
+++ script.js
@@ -0,0 +1,11 @@
+IMask(document.querySelector("#cc-cvv"), {
+  mask: "0000",
+});
+
+IMask(document.querySelector("#cc-number"), {
+  mask: "0000 0000 0000 0000",
+});
+
+IMask(document.querySelector("#cc-number"), {
+  mask: "0000 0000 0000 0000",
+});
